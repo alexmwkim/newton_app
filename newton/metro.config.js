004fb6184e25fff62acfa096e7118a01e99f4843
alex kim <alexmwkim@gmail.com>
2025-06-27 15:36:15 -0400

    Ensure newton folder is completely removed

--- newton/metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Simplified Metro config
-config.resolver.platforms = ['ios', 'android', 'native'];
-
-module.exports = config;
